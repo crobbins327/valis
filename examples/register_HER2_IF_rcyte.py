ecede7fb367810afacc3dbf260b291176bca362f
crobbins327 <crobbins327@gmail.com>
2022-09-18 17:26:19 -0400

    run on cluster

--- examples/register_HER2_IF_rcyte.py
+++ examples/register_HER2_IF_rcyte.py
@@ -11,12 +11,12 @@ from valis.slide_io import VipsSlideReader, BioFormatsSlideReader
 # results_dst_dir = "/content/drive/MyDrive/Yale-QIF-HER2/H&E/expected_results/registration"
 
 serial_folders = [
-                'S18_26442_0_all',
+                # 'S18_26442_0_all',
                 # 'S18_28210_2_all',
-                # 'S18_31022_2_all',
-                # 'S19_08281_1_all',
-                # 'S19_09265_2N_all',
-                # 'S19_09851_0_all',
+                'S18_31022_2_all',
+                'S19_08281_1_all',
+                'S19_09265_2N_all',
+                'S19_09851_0_all'
                 # 'S20_15425_all',
                 # 'S20_18310_0_all',
                 # 'S20_21308_all',
@@ -24,7 +24,8 @@ serial_folders = [
                 # 'S20_25677_all',
                 # 'S20_25771_all'
                   ]
-main_dir = "G:/My Drive/Yale-QIF-HER2/SerialSection_IntraAssay_RCyte/5ms"
+#main_dir = "G:/My Drive/Yale-QIF-HER2/SerialSection_IntraAssay_RCyte/5ms"
+main_dir = "/home/cjr66/scratch60/HS-HER2"
 DEFAULT_FLOURESCENCE_PROCESSING_ARGS = {"channel": "DAPI", "adaptive_eq": True}
 
 for cf in serial_folders:
@@ -34,21 +35,21 @@ for cf in serial_folders:
 
   # Create a Valis object and use it to register the slides in slide_src_dir
   start = time.time()
-  registrar = registration.Valis(slide_src_dir, results_dst_dir, imgs_ordered=True, max_image_dim_px=2000)
+  registrar = registration.Valis(slide_src_dir, results_dst_dir, imgs_ordered=True)
   # reader_cls = VipsSlideReader
   rigid_registrar, non_rigid_registrar, error_df = registrar.register(if_processing_kwargs=DEFAULT_FLOURESCENCE_PROCESSING_ARGS)
   stop = time.time()
   elapsed = stop - start
-  print(f"regisration time is {elapsed/60} minutes")
+  print("regisration time is {} minutes".format(elapsed/60))
 
   # Check results in registered_slide_dst_dir. If they look good, export the registered slides
   registered_slide_dst_dir = os.path.join(main_dir, "expected_results/nonrigid_registered_slides", registrar.name)
   # registered_slide_dst_dir = os.path.join(main_dir, "expected_results/rigid_registered_slides", registrar.name)
   start = time.time()
-  registrar.warp_and_save_slides(registered_slide_dst_dir, non_rigid = True, compression='jpg2k')
+  registrar.warp_and_save_slides(registered_slide_dst_dir, non_rigid = True, perceputally_uniform_channel_colors=True, compression='jp2k')
   stop = time.time()
   elapsed = stop - start
-  print(f"saving {registrar.size} slides took {elapsed/60} minutes")
+  print("saving {} slides took {} minutes".format(registrar.size, elapsed/60))
 
 
 
